9d447b0902c8747d2a3c36a6b595112e4b0ae980
William Neild <wneild5@gmail.com>
2023-06-26 16:35:01 +0100

    feat: bundle exported next application and serve from within mod

--- src/main/java/com/williamneild/SimpleStats.java
+++ src/main/java/com/williamneild/SimpleStats.java
@@ -10,6 +10,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,9 +18,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 
 public class SimpleStats implements ModInitializer {
@@ -36,8 +41,47 @@ public class SimpleStats implements ModInitializer {
 		File configDirFile = new File(configDir.toUri());
 		if (!configDirFile.exists()) configDirFile.mkdirs();
 
+		// update web app if needed
+		extractWebApp(Paths.get(String.valueOf(configDir), "web"));
+
 		// init web server when server starts
 		ServerLifecycleEvents.SERVER_STARTED.register(this::initialiseWebServer);
+	}
+
+	private void extractWebApp(Path webAppPath) {
+		File webAppDir = new File(webAppPath.toUri());
+		if (webAppDir.exists()) return;
+
+		// create dir
+		if (!webAppDir.mkdirs()) return;
+
+		// get the web archive
+		try {
+			URL resource = getClass().getResource("/assets/simple-stats/web.zip");
+			File tempFile = File.createTempFile("bluemap_webroot_extraction", null);
+			if (resource == null) {
+				LOGGER.error("Error getting web app zip");
+				return;
+			}
+
+			FileUtils.copyURLToFile(resource, tempFile, 10000, 10000);
+			try (ZipFile zipFile = new ZipFile(tempFile)) {
+				Enumeration<? extends ZipEntry> entries = zipFile.entries();
+				while (entries.hasMoreElements()) {
+					ZipEntry zipEntry = entries.nextElement();
+					if (zipEntry.isDirectory()) {
+						File dir = webAppPath.resolve(zipEntry.getName()).toFile();
+						FileUtils.forceMkdir(dir);
+					} else {
+						File target = webAppPath.resolve(zipEntry.getName()).toFile();
+						FileUtils.forceMkdirParent(target);
+						FileUtils.copyInputStreamToFile(zipFile.getInputStream(zipEntry), target);
+					}
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.error("Error", e);
+		}
 	}
 
 	private void initialiseWebServer(MinecraftServer minecraftServer) {
@@ -48,7 +92,7 @@ public class SimpleStats implements ModInitializer {
 		RoutingRequestHandler routingRequestHandler = new RoutingRequestHandler();
 
 		// default route
-		Path dir = Paths.get(FabricLoader.getInstance().getConfigDir().toString(), "simplestats");
+		Path dir = Paths.get(FabricLoader.getInstance().getConfigDir().toString(), "simplestats", "web");
 		routingRequestHandler.register(".*", new FileRequestHandler(dir));
 
 		routingRequestHandler.register("bulk", new JsonDataRequestHandler(new BulkSupplier(minecraftServer)));
